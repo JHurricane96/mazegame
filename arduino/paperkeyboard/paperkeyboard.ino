c10ebc07678f1f66b467e97ed65540a84f9cc3e0
thakkarparth007 <thakkarparth007@gmail.com>
2016-05-05 15:15:13 +0530

    Add arduino code to its directory to satisfy Arduino IDE

--- arduino/paperkeyboard/paperkeyboard.ino
+++ arduino/paperkeyboard/paperkeyboard.ino
@@ -1,6 +1,6 @@
-int leftPin = A0;
+int leftPin = A15;
 int upPin = A4;
-int rightPin = A8;
+int rightPin = A9;
 int downPin = A12;
 
 void setup() {
@@ -11,26 +11,32 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(A1,LOW);
-  digitalWrite(A3,LOW);
+//  digitalWrite(A1,LOW);
+//  digitalWrite(A3,LOW);
   digitalWrite(leftPin,LOW);
   Serial.print(digitalRead(leftPin));
   Serial.print(",");
 
   delay(50);
 
+  digitalWrite(A3,LOW);
+  digitalWrite(A5,LOW);
   digitalWrite(upPin,LOW);
   Serial.print(digitalRead(upPin));
   Serial.print(",");
 
   delay(50);
-  
+
+  digitalWrite(A8,LOW);
+  digitalWrite(A10,LOW);
   digitalWrite(rightPin,LOW);
   Serial.print(digitalRead(rightPin));
   Serial.print(",");
 
   delay(50);
 
+  digitalWrite(A11,LOW);
+  digitalWrite(A13,LOW);  
   digitalWrite(downPin,LOW);
   Serial.print(digitalRead(downPin));
   Serial.print("\n");
